78ead37aa5885d91ee666c0124fbac1dfb6782db
Boggle-Head <b.smithers.actual@gmail.com>
2019-10-23 11:49:10 -0500

    Added more operators, constructors for both position and direction

--- private/LeptonInjector/Coordinates.cxx
+++ private/LeptonInjector/Coordinates.cxx
@@ -1,4 +1,4 @@
-#include<LeptonInjector/Helper.h>
+#include<LeptonInjector/Coordinates.h>
 #include <array>
 
 namespace LeptonInjector {
@@ -13,78 +13,97 @@ namespace LeptonInjector {
 		return(rotated);
 	}
 
-	std::array<double, n_dimensions> RotateY(std::array<double, n_dimensions> vector, double angle) {
-		std::array<double, n_dimensions> rotated = { vector[0]*cos(angle)+y*sin(angle), -1*vector[0]*sin(angle)+vector[1]*cos(angle), vector[2] };
+	std::array<double, n_dimensions> RotateZ(std::array<double, n_dimensions> vector, double angle) {
+		std::array<double, n_dimensions> rotated = { vector[0]*cos(angle)+vector[1]*sin(angle), -1*vector[0]*sin(angle)+vector[1]*cos(angle), vector[2] };
 		return(rotated);
 	}
 
 
 
 	// create a "direction" object to use with some of the LI dependencies 
+
+	// default constructor, just zeroes 
 	LI_Direction::LI_Direction(){
 		this->zenith 	= 0.0;
 		this->azimuth 	= 0.0;
 	}
 
-	LI_Direction::~LI_Direction(){
-		// ... ?
-	}
-
+	// takes two doubles, {zenith, azimuth}
 	LI_Direction::LI_Direction( double theta, double phi){
 		this->zenith = theta;
 		this->azimuth = phi;
-
 		// if the zenith angle is too 
 		while( this->azimuth >= 2*Constants::pi ){
 			this->azimuth -= 2*Constants::pi;
 		}
-
+	}
+	// accepts an array {zenith, azimuth}
+	LI_Direction::LI_Direction(std::array<double, 2> dir ){
+		this->zenith = dir[0];
+		this->azimuth = dir[1];
 	}
 
+	// can also take a "pair" of doubles
 	LI_Direction::LI_Direction( std::pair<double, double> dir){
 		this->zenith = dir.first;
 		this->azimuth = dir.second;
 	}
+	LI_Direction::LI_Direction(LI_Direction& old_one){
+		this->zenith = old_one.zenith;
+		this->azimuth = old_one.azimuth;
+	}
+	// construct a direction for a given vector 
+	LI_Direction::LI_Direction( LI_Position& vec){
+		this->azimuth = atan( vec.at(1)/ vec.at(0) );
+		this->zenith  = acos( vec.at(2)/ vec.Magnitude() );
+	}
 
 
 
-	// create a 
-	// define the LI_Position constructors 
+	// define the LI_Position constructors and member functions 
+
+	// default constructor sets all n components to zero
 	LI_Position::LI_Position(){
 		for (uint8_t iter =0; iter<n_dimensions; iter++){
 			this->position[iter] = 0.0;
 		}
 	}
-
-	LI_Position::~LI_Position(){
-	}
-
+	// uses old position to make a new position
 	LI_Position::LI_Position(LI_Position& old_one){
 		this->position = old_one.position;
 	}
-
+	// uses an array to construct a position 
 	LI_Position::LI_Position(std::array<double, n_dimensions> pos){
 		for (uint8_t iter =0; iter<n_dimensions; iter++){
 			this->position[iter] = pos[iter];
 		}
 	}
-
+	// returns the value of a specified component 
 	double LI_Position::at( uint8_t component){
+		// because component is unsigned, the only achievable values are >0 and <255. So, we just check that it's 
+		// 		under the dimensionality of the arrays 
+		if(component >= n_dimensions){
+			// throw an exception. 
+			throw std::out_of_range("Invalid component requested");
+		}
+
 		return( this->position[component] );
 	}
 
+	// returns the magnitude of the position vector
 	double LI_Position::Magnitude(){
 		double mag = 0.0;
+		// note that the pythagorean theorem trivially generalizes to n_dim>2
 		for (uint8_t iter=0; iter<n_dimensions; iter++){
-			mag += pow( this->at(iter), 2);
+			mag += pow( this->at(iter), 2); //sum the squares
 		}
 
-		return( sqrt(mag) );
+		return( sqrt(mag) ); //return the sum's root
 	}
 
 	// need to overload some operations on the newly formed LI_Direction and LI_Position
 
-	// takes a LI_Position (basically a vector), and scales each component by a constant. 
+	// this one scales a position vector by a constant
 	LI_Position operator * (LI_Position const &point, double scalar){
 		std::array<double, n_dimensions> new_one; 
 		for (uint8_t iter  = 0; iter<n_dimensions; iter++){
@@ -93,7 +112,16 @@ namespace LeptonInjector {
 		return(LI_Position( new_one ));
 	}
 
-	// define the dot product between a vector and a direction. 
+	// when multiplying a direction by a scalar, you are left with a vector 
+	LI_Position operator * (LI_Direction const &dir, double scalar){
+		// calculate the coordinates
+		double ex = scalar*cos(dir.azimuth)*sin(dir.zenith);
+		double why = scalar*sin(dir.zenith)*sin(dir.azimuth);
+		double zee = scalar*cos(dir.zenith);
+		return( LI_Position(ex, why, zee) );
+	}
+
+	// define the dot product between a vector and a direction. The direction is first turned into a unit vector
 	double operator * (LI_Position const &pos, LI_Direction const &dir){
 		// construct effective position for a unit vector in the direction of dir
 		std::array<double,n_dimensions> new_dir = { cos(dir.azimuth)*sin(dir.zenith), sin(dir.azimuth)*sin(dir.zenith), cos(dir.zenith)};
@@ -107,14 +135,21 @@ namespace LeptonInjector {
 		return( projected );
 	}
 
+	// similar to above, this calculates the dot product of two position vectors 
+	double operator * (LI_Position const &vec1, LI_Position const &vec2){
+		double dot_prod = 0;
+		for (uint8_t iter=0; iter<n_dimensions; iter++){
+			dot_prod += vec1.at(iter)*vec2.at(iter);
+		}
+		return( dot_prod );
+	}
+
 	// implement adding and subtracting positions. Like vector addition! 
 	LI_Position operator + (LI_Position const &pos1, LI_Position const &pos2){
 		std::array<double, n_dimensions> new_one;
-
 		for (uint8_t iter=0; iter<n_dimensions; iter++){
 			new_one[iter] = pos1.at(iter) + pos2.at(iter);
 		}
-
 		return( LI_Position( new_one ) );
 	}
 	LI_Position operator - (LI_Position const &pos1, LI_Position const &pos2){
@@ -125,6 +160,15 @@ namespace LeptonInjector {
 		}
 
 		return( LI_Position( new_one ) );
+	}
+
+	LI_Position& operator += (LI_Position& one, LI_Position& two){
+		one = one + two;
+		return( one );
+	}
+	LI_Position& operator += (LI_Position& one, LI_Position& two){
+		one = one - two;
+		return( one );
 	}
 
 } // end namespace LeptonInjector
--- public/LeptonInjector/Coordinates.h
+++ public/LeptonInjector/Coordinates.h
@@ -1,15 +1,21 @@
-#ifndef LI_HELPER
-#define LI_HELPER
+#ifndef LI_COORDS
+#define LI_COORDS
 
-#include <array>
+#include <array> // used for many of the constructors, and for the position
 #include <math.h> //sqrt, sin, cos, pow
+#include <exception> //allows throwing the out_of_range exception
 #include <LeptonInjector/Constants.h>
 
+// Implements tools and classes for working with positions and directions
+
+// Ben Smithers
+// benjamin.smithers@mavs.uta.edu
 
 namespace LeptonInjector {
 
 	// how many linearly independent bases do we need to span our space ?
 	// this should always be 3
+	// but... maybe one day we want more dimensions, and don't want to have to rewrite all of this? 
 	static const int n_dimensions = 3;
 
 
@@ -19,30 +25,39 @@ namespace LeptonInjector {
 	std::array<double, n_dimensions> RotateX(std::array<double, n_dimensions> vector, double angle);
 	std::array<double, n_dimensions> RotateZ(std::array<double, n_dimensions> vector, double angle);
 
+	// Creating a "LI_Direction" to mimic the I3_Direction object
+	// this should behave in the same exact way, at least within the scope of this project 
 	class LI_Direction{
 		public:
 			LI_Direction();
-			~LI_Direction();
+			virtual ~LI_Direction();
 
 			LI_Direction( double theta, double phi);
+			LI_Direction( std::array<double, 2> dir);
 			LI_Direction( std::pair<double, double> dir);
+			LI_Direction( LI_Direction& old_one);
+			LI_Direction( LI_Position& vec);  // get the direction of a vector 
 
 			
 			double zenith;
 			double azimuth;
 	};
 
+	// Creating a "LI_Position" to mimic the I3_Position object
+	// this is more accurately described as a vector in cartesian coordinates
+	//
+	// **IMPORTANT** : member functions and operations are defined using **cartesian** coordinates.
+	// 					keep this in mind when working with these coordinates! 
 	class LI_Position{
 		public: 
 			LI_Position();
-			~LI_Position();
+			virtual ~LI_Position();
 
 			LI_Position(double ex, double why, double zee);
 			LI_Position(LI_Position& old_one);
 			LI_Position(std::array<double, n_dimensions> pos);
 
 			double at(uint8_t component);
-
 			double Magnitude();
 
 		private:
