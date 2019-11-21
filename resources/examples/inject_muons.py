1cd9ae916ff3bac4912b2fb09bd32128e90029c9
Boggle-Head <b.smithers.actual@gmail.com>
2019-11-21 15:57:25 -0600

    include multiple injectors in the example

--- resources/examples/inject_muons.py
+++ resources/examples/inject_muons.py
@@ -11,14 +11,18 @@ import pylepton_injector as LI
 # define the injector 
 final_type1 = LI.Particle.MuPlus
 final_type2 = LI.Particle.Hadrons
-n_events    = 100
+n_events    = 50000
 diff_xs     = "/home/benito/software/cross_sections/dsdxdy_nubar_CC_iso.fits"
 total_xs    = "/home/benito/software/cross_sections/sigma_nubar_CC_iso.fits"
-is_ranged   = True # use the ranged mode injection
+is_ranged   = True
+#is_ranged   = True # use the ranged mode injection
 # because the final state had a charged muon and hadrons, the event is the result of an antimuon neutrino undergoing a charged current interaction
 
 # create the injector list using the above parameters
 the_injector = LI.injector( n_events, final_type1, final_type2, diff_xs, total_xs, is_ranged)
+
+the_next_injector = LI.injector( n_events , LI.Particle.NuEBar, LI.Particle.Hadrons, diff_xs, total_xs, True)
+
 
 deg = 3./180.
 
@@ -32,6 +36,7 @@ maxAzimuth  = 180.*deg
 
 # construct the controller 
 controller  = LI.Controller( the_injector, minE, maxE, gamma, minAzimuth, maxAzimuth, minZenith, maxZenith)  
+controller.AddInjector( the_next_injector )
 # injection radius and endcap length are left as defaults
 
 # specify the output
