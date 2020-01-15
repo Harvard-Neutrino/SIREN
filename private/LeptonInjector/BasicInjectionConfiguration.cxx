e31420c4e5da6bade305c9529ec44141c66305fd
Boggle-Head <b.smithers.actual@gmail.com>
2020-01-15 15:54:26 -0600

    Moved this out of the main LI file to prevent circular dependency

---
+++ private/LeptonInjector/BasicInjectionConfiguration.cxx
@@ -0,0 +1,23 @@
+
+#include <BasicInjectionConfiguration.h>
+
+namespace LeptonInjector{
+
+	BasicInjectionConfiguration::BasicInjectionConfiguration():
+	events(1),
+	energyMinimum(10*Constants::GeV),
+	energyMaximum((1e9)*Constants::GeV),
+	powerlawIndex(1.0),
+	azimuthMinimum(0),
+	azimuthMaximum(2*Constants::pi),
+	zenithMinimum(0),
+	zenithMaximum(Constants::pi),
+	finalType1(ParticleType::MuMinus),
+	finalType2(ParticleType::Hadrons),
+	injectionRadius(1200*LeptonInjector::Constants::m),
+	endcapLength(1200*LeptonInjector::Constants::m),
+	cylinderRadius(1200*LeptonInjector::Constants::m),
+	cylinderHeight(1200*LeptonInjector::Constants::m)
+	{}
+
+} // end namespace LeptonInjector
