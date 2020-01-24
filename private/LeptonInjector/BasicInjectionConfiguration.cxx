1b43f325958cf2afc75ce0885b3a7d59484d2c1d
Boggle-Head <b.smithers.actual@gmail.com>
2020-01-24 14:25:21 -0600

    added function for setting the cross section blobs

--- private/LeptonInjector/BasicInjectionConfiguration.cxx
+++ private/LeptonInjector/BasicInjectionConfiguration.cxx
@@ -21,33 +21,34 @@ namespace LeptonInjector{
 	cylinderHeight(1200*LeptonInjector::Constants::m)
 	{}
 
-	/*
+	
 	void BasicInjectionConfiguration::setCrossSection(const photospline::splinetable<>& crossSection, const photospline::splinetable<>& totalCrossSection){
 		splinetable_buffer buf;
 		buf.size=0;
 		//buf.mem_alloc=&malloc;
 		//buf.mem_realloc=&realloc;
-		int result=writesplinefitstable_mem(&buf, &crossSection );
-		if(result!=0){
-			free(buf.data);
-			std::cout << "photospline error while serializing cross section: " << result << std::endl;
-			throw;
-		}
+
+		// int result=writesplinefitstable_mem(&buf, &crossSection. );
+		// auto& real_table=*static_cast<photospline::splinetable<>*>(crossSection->data);
+		auto result_obj = crossSection.write_fits_mem();
+		buf.data=result_obj.first;
+		buf.size=result_obj.second;
+
+
 		crossSectionBlob.resize(buf.size);
 		std::copy((char*)buf.data,(char*)buf.data+buf.size,&crossSectionBlob[0]);
 		free(buf.data);
 		
 		buf.size=0;
-		result=writesplinefitstable_mem(&buf, &totalCrossSection);
-		if(result!=0){
-			free(buf.data);
-			std::cout << "photospline error while serializing cross section: " << result << std::endl;
-			throw;
-		}
+		result_obj = totalCrossSection.write_fits_mem();
+		buf.data = result_obj.first;
+		buf.size = result_obj.second;
+		// result=writesplinefitstable_mem(&buf, &totalCrossSection);
+
 		totalCrossSectionBlob.resize(buf.size);
 		std::copy((char*)buf.data,(char*)buf.data+buf.size,&totalCrossSectionBlob[0]);
 		free(buf.data);
 	}
-*/
+
 
 } // end namespace LeptonInjector
