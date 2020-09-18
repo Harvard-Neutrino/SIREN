9ce7936165c43a721b90e9b133aa3f7d9b414ea9
Austin Schneider <hogenshpogen@gmail.com>
2020-09-18 17:26:36 -0400

    Avoid floating point errors by using atan2 instead of atan.

--- private/LeptonInjector/Coordinates.cxx
+++ private/LeptonInjector/Coordinates.cxx
@@ -41,7 +41,7 @@ namespace LeptonInjector {
 
 	// construct a direction for a given vector 
 	LI_Direction::LI_Direction( const LI_Position& vec){
-		this->azimuth = atan( vec.at(1)/ vec.at(0) );
+		this->azimuth = atan2( vec.at(1), vec.at(0) );
 		this->zenith  = acos( vec.at(2)/ vec.Magnitude() );
 	}
 
