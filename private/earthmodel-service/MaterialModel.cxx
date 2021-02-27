34fdb0bbcb7bcbf7bf1464de0cc77965f173a6bd
Austin Schneider <hogenshpogen@gmail.com>
2021-02-27 11:40:15 -0500

    Add MaterialModel class

---
+++ private/earthmodel-service/MaterialModel.cxx
@@ -0,0 +1,149 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <earthmodel-service/MaterialModel.h>
+
+using namespace earthmodel;
+
+MaterialModel::MaterialModel() {}
+
+MaterialModel::MaterialModel(std::string const & path) : path_(path) {}
+
+MaterialModel::MaterialModel(std::string const & path, std::string const & matratio) : path_(path) {
+    AddModelFile(matratio);
+}
+
+MaterialModel::MaterialModel(std::string const & path, std::vector<std::string> const & matratios) : path_(path) {
+    AddModelFiles(matratios);
+}
+
+void MaterialModel::SetPath(std::string const & path) {
+    path_ = path;
+}
+
+void MaterialModel::AddModelFiles(std::vector<std::string> const & matratios) {
+    for(auto matratio : matratios)
+        AddModelFile(matratio);
+}
+
+void MaterialModel::AddModelFile(std::string matratio) {
+    if (matratio.find(".dat") == std::string::npos)
+        matratio += ".dat";
+
+    // check earthmodel file
+    std::string fname = (matratio.find('/')==std::string::npos ? path_ + "materials/" + matratio : matratio);
+    std::ifstream in(fname.c_str(), std::ifstream::in);
+
+    if (in.fail())
+        throw("failed to open " + fname + ". Set correct path.");
+
+    // read the file
+    const int bufsize = CHAR_BUF_SIZE;
+    char buf[bufsize];
+    std::string medtype;
+    int matpdg, nmats;
+    double weight;
+    int nread = 0;
+
+    while(!in.eof()) {
+
+        in.getline(buf, bufsize);
+        nread = in.gcount();
+
+        if(nread == -1) {
+            throw("getline failed");
+
+        } else if(nread == 1 || buf[0] == ' ' || buf[0] == '#') {
+            // new line, start from white space, or comment line.
+            continue;
+        } else {
+            // material density data
+            std::stringstream ss(buf);
+
+            ss >> medtype >> nmats;
+
+            std::map<int, double> matratio;
+            for(int i=0; i<nmats; ++i) {
+                in.getline(buf, bufsize);
+                nread = in.gcount();
+                if(nread == -1) {
+                    throw("getline failed");
+                } else if(nread == 1 || buf[0] == ' ' || buf[0] == '#') {
+                    // new line, start from white space, or comment line.
+                    --i;
+                    continue;
+                } else {
+                    std::stringstream ss2(buf);
+                    ss2 >> matpdg >> weight;
+                    matratio[matpdg] = weight;
+                }
+            }
+            int id = material_names_.size();
+            material_ids_.insert({medtype, id});
+            material_names_.push_back(medtype);
+            material_maps_.insert({id, matratio});
+            pne_ratios_.insert({id, ComputePNERatio(id)});
+        }
+
+    } // end of the while loop
+    model_files_.push_back(matratio);
+
+    in.close();
+}
+
+double MaterialModel::ComputePNERatio(int id) {
+    // calculate P, N, E ratio
+    std::map<int, double> &mats = material_maps_[id];
+    double tot_np = 0;
+    double tot_nn = 0;
+    int np, nn;
+    for(auto const & it : mats) {
+        int pdg = it.first;
+        GetAZ(pdg, np, nn);
+        tot_np += np;
+        tot_nn += nn;
+    }
+
+    int tot_z = tot_np + tot_nn;
+    if(tot_z==0)
+        tot_z=1; //avoid division by zero
+
+    double nw_proton = tot_np / tot_z;
+    double nw_electron = nw_proton;
+    //double nw_neutron = tot_nn / tot_z;
+
+    return nw_electron;
+}
+
+double MaterialModel::GetPNERatio(int id) {
+    return pne_ratios_[id];
+}
+
+std::string MaterialModel::GetMaterialName(int id) {
+    return material_names_[id];
+}
+
+int MaterialModel::GetMaterialId(std::string const & name) {
+    return material_ids_[name];
+}
+
+void MaterialModel::GetAZ(int code, int & np, int & nn) {
+    np = 0;
+    int z = 0;
+    int prefix = 0;
+    int suffix = 0;
+
+    char buf[CHAR_BUF_SIZE];
+    sprintf(buf, "%d", code);
+    int nread = sscanf(buf, "%3d%3d%3d%1d", &prefix, &np, &z, &suffix);
+    if (nread != 4) {
+        throw std::runtime_error("Failed to convert nuclear pdg to A and Z "
+                "prefix "+std::to_string(prefix)+", A "+std::to_string(np)+", Z "+std::to_string(z)+", suffix "+std::to_string(suffix));
+    }
+    nn = z - np;
+}
+
---
+++ public/earthmodel-service/MaterialModel.h
@@ -0,0 +1,41 @@
+#ifndef LI_MaterialModel_H
+#define LI_MaterialModel_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace earthmodel {
+
+class MaterialModel {
+    static constexpr const int CHAR_BUF_SIZE = 8196;
+private:
+    std::string path_;
+    std::vector<std::string> model_files_;
+
+    std::vector<std::string> material_names_;
+    std::map<std::string, int> material_ids_;
+    std::map<int, std::map<int, double> > material_maps_;
+    std::map<int, double> pne_ratios_;
+public:
+    MaterialModel();
+    MaterialModel(std::string const & path);
+    MaterialModel(std::string const & path, std::string const & matratio);
+    MaterialModel(std::string const & path, std::vector<std::string> const & matratios);
+
+    void SetPath(std::string const & path);
+    void AddModelFiles(std::vector<std::string> const & matratios);
+    void AddModelFile(std::string matratio);
+
+    double GetPNERatio(int id);
+    std::string GetMaterialName(int id);
+    int GetMaterialId(std::string const & name);
+private:
+    double ComputePNERatio(int id);
+public:
+    static void GetAZ(int code, int & np, int & nn);
+};
+
+} // namespace earthmodel
+
+# endif // LI_MaterialModel_H
