464f6d36ddae5040ac884e5e31bc8ad1595d97be
Austin Schneider <hogenshpogen@gmail.com>
2021-03-23 08:37:07 -0400

    Move MaterialsModel test setup into separate header for use in EarthModel tests.

---
+++ tests/EarthModel_TEST.cxx
@@ -0,0 +1,31 @@
+
+#include <cmath>
+#include <math.h>
+#include <cstdio>
+#include <string>
+#include <random>
+#include <fstream>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <earthmodel-service/EarthModel.h>
+#include "MaterialFake.h"
+
+using namespace earthmodel;
+
+TEST(Constructor, Default)
+{
+    EarthModel A;
+}
+
+TEST_F(MaterialTest, EarthModelConstructor)
+{
+    EarthModel A("", materials_file, "");
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
