0640b2e5ba59f6e858fa2a9024da77b555fe337a
Austin Schneider <hogenshpogen@gmail.com>
2022-02-28 09:45:40 -0500

    Move interpolator to it's own file.

---
+++ private/phys-services/Interpolator.cxx
@@ -0,0 +1,30 @@
+#include <tuple>
+
+#include "phys-services/Interpolator.h"
+
+using namespace LeptonInjector;
+
+template<>
+template<>
+bool TableData1D<double>::operator==<double>(TableData1D<double> const & other) const {
+    return std::tie(x, f) == std::tie(other.x, other.f);
+}
+
+template<>
+template<>
+bool TableData2D<double>::operator==<double>(TableData2D<double> const & other) const {
+    return std::tie(x, y, f) == std::tie(other.x, other.y, other.f);
+}
+
+template<>
+template<>
+bool Interpolator1D<double>::operator==<double>(Interpolator1D<double> const & other) const {
+    return original_table == other.original_table;
+}
+
+template<>
+template<>
+bool Interpolator2D<double>::operator==<double>(Interpolator2D<double> const & other) const {
+    return original_table == other.original_table;
+}
+
