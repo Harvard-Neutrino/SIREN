23ee22d42f5e231c8f0ee7d7e2478cc0d87b4853
Austin Schneider <hogenshpogen@gmail.com>
2023-04-17 14:30:20 -0500

    Add cfitsio compatibility for i686 image

---
+++ tools/get_plat.py
@@ -0,0 +1,28 @@
+import sysconfig
+import platform
+
+SUPPORTED_PLATFORMS = [
+    'linux-aarch64',
+    'linux-x86_64',
+    'musllinux-x86_64',
+    'linux-i686',
+    'linux-ppc64le',
+    'linux-s390x',
+    'win-amd64',
+    'win-32',
+    'macosx-x86_64',
+    'macosx-arm64',
+]
+
+def get_plat():
+    plat = sysconfig.get_platform()
+    plat_split = plat.split("-")
+    arch = plat_split[-1]
+    if arch == "win32":
+        plat = "win-32"
+    elif arch in ["universal2", "intel"]:
+        plat = f"macosx-{platform.uname().machine}"
+    elif len(plat_split) > 2:
+        plat = f"{plat_split[0]}-{arch}"
+    assert plat in SUPPORTED_PLATFORMS,  f'invalid platform {plat}'
+    return plat
---
+++ tools/wheels/cibw_before_all.sh
@@ -0,0 +1,24 @@
+set -xe
+
+PROJECT_DIR="$1"
+
+PLATFORM=$(PYTHONPATH=tools python -c "import get_plat; print(get_plat.get_plat())")
+
+pip install scikit-build-core
+
+if [[ $PLATFORM == "linux-x86_64" ]]
+then
+    yum install -y cfitsio-devel
+elif [[ $PLATFORM == "linux-i686" ]]
+then
+    mkdir /tmp/downloads
+    cd /tmp/downloads
+    curl https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz --output cfitsio-4.2.0.tar.gz
+    tar -xvf cfitsio-4.2.0.tar.gz
+    cd cfitsio-4.2.0
+    mkdir build
+    cd build
+    cmake ../
+    make
+    make install
+fi
