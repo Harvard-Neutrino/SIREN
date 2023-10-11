ab70ee145c6689b96f41dd9149f709a6e7138496
Nicholas Kamp <nkamp@fas.harvard.edu>
2023-10-11 18:48:27 -0400

    SampleFinalState implemented for DarkNews xsec

--- projects/crosssections/private/DarkNewsCrossSection.cxx
+++ projects/crosssections/private/DarkNewsCrossSection.cxx
@@ -52,18 +52,12 @@ double DarkNewsCrossSection::DifferentialCrossSection(dataclasses::InteractionRe
     rk::P4 p2(geom3::Vector3(interaction.target_momentum[1], interaction.target_momentum[2], interaction.target_momentum[3]), interaction.target_mass);
     rk::P4 p3(geom3::Vector3(interaction.secondary_momenta[0][1], interaction.secondary_momenta[0][2], interaction.secondary_momenta[0][3]), interaction.secondary_masses[0]);
 
-    // we assume that:
-    // the target is stationary so its energy is just its mass
-    // the incoming neutrino is massless, so its kinetic energy is its total energy
-    // double s = target_mass_ * target_mass_ + 2 * target_mass_ * primary_energy;
-    double s = std::pow(rk::invMass(p1, p2), 2);
-
     double primary_energy;
     rk::P4 p1_lab;
-    rk::P4 p2_lab;
+    rk::P4 p3_lab;
     if(interaction.target_momentum[1] == 0 and interaction.target_momentum[2] == 0 and interaction.target_momentum[3] == 0) {
         p1_lab = p1;
-        p2_lab = p2;
+        p3_lab = p2;
         primary_energy = p1_lab.e();
     } else {
         rk::Boost boost_start_to_lab = p2.restBoost();
@@ -118,6 +112,13 @@ void DarkNewsCrossSection::SampleFinalState(dataclasses::InteractionRecord & int
     // we assume that:
     // the target is stationary so its energy is just its mass
     // the incoming neutrino is massless, so its kinetic energy is its total energy
+    double s = std::pow(rk::invMass(p1, p2), 2);
+
+    // define masses that we will use
+    double m1 = interaction.primary_mass;
+    double m2 = interaction.target_mass;
+    double m3 = interaction.secondary_mass[0];
+    double m4 = interaction.secondary_mass[1];
 
     double primary_energy;
     rk::P4 p1_lab;
@@ -152,8 +153,87 @@ void DarkNewsCrossSection::SampleFinalState(dataclasses::InteractionRecord & int
     // sample an intial point
     kin_vars[1] = std::pow(10,random->Uniform(log_minQ2,log_maxQ2));
     
+    test_cross_section = DifferentialCrossSection(interaction.signature.primary_type interaction.signature.target_type, primary_energy, kin_vars[1])
+    cross_section = test_cross_section
 
-    
+    // this is the magic part. Metropolis Hastings Algorithm.
+    // MCMC method!
+    const size_t burnin = 40; // converges to the correct distribution over multiple samplings.
+                              // big number means more accurate, but slower
+    for(size_t j = 0; j <= burnin; j++) {
+        // repeat the sampling from above to get a new valid point
+        test_kin_vars[1] = std::pow(10,random->Uniform(log_minQ2,log_maxQ2));
+        test_cross_section = DifferentialCrossSection(interaction.signature.primary_type interaction.signature.target_type, primary_energy, kin_vars[1])
+        double odds = (test_cross_section / cross_section);
+        accept = (cross_section == 0 || (odds > 1.) || random->Uniform(0, 1) < odds);
+
+        if(accept) {
+            kin_vars = test_kin_vars;
+            cross_section = test_cross_section;
+        }
+    }
+    double final_Q2 = kin_vars[1];
+
+    // Working in center of mass frame, assuming 2 -> 2 scattering
+    double E1CM = (s + pow(interaction.primary_mass,2) - pow(interaction.target_mass,2)) / (2*sqrt(s));
+    double E3CM = (s + pow(interaction.secondary_masses[0],2) - pow(interaction.secondary_masses[1],2)) / (2*sqrt(s));
+    double P1CM = sqrt(E1CM*E1CM - pow(interaction.primary_mass,2));
+    double P3CM = sqrt(E3CM*E3CM - pow(interaction.secondary_masses[0],2));
+                 
+    double CosThetaCM = (final_Q2 
+                         + pow(interaction.primary_mass,2) 
+                         + pow(interaction.secondary_masses[0],2)
+                         - 2*E1CM*E3CM)
+                        / (-2*P1CM*P3CM);
+
+    // Leverage the fact that the target is at rest
+    double E4Lab = (final_Q2 + pow(m2,2) + pow(m4,2)) / (2*m2);
+    double E3Lab = primary_energy + p2_lab.e() - E4Lab;
+    double P1Lab = sqrt(primary_energy*primary_energy - pow(m1,2));
+    double P3Lab = sqrt(E3Lab*E3Lab - pow(m3,2));
+    double CosThetaLab = (primary_energy*E3Lab - 0.5*(final_Q2 + m1*m1 + m3*m3))/(P1Lab*P3Lab);
+    double PhiLab = random->Uniform(0, 2.0 * M_PI);
+
+    // Apply scattering angle in the x direction, then rotate to
+    // incoming particle direction
+    geom3::UnitVector3 x_dir = geom3::UnitVector3::xAxis();
+    geom3::Vectjor3 p1_mom = p1_lab.momentum();
+    geom3::UnitVector3 p1_lab_dir = p1_mom.direction();
+    geom3::Rotation3 x_to_p1_lab_rot = geom3::rotationBetween(x_dir, p1_lab_dir);
+    geom3::Rotation3 rand_rot(p1_lab_dir, PhiLab);
+
+    rk::P4 p3_lab(E3Lab,P1Lab*geom3::Vector3([CosThetaLab,sqrt(1-CosThetaLab*CosThetaLab),0]));
+    p3_lab.rotate(x_to_p1_lab_rot);
+    p3_lab.rotate(rand_rot);
+
+    // Rotate back to whatever frame the traget was in originally.
+    // I believe we have identified the lab frame as the target 
+    // rest ferame in this function
+    rk::P4 p3;
+    rk::P4 p4;
+    if(interaction.target_momentum[1] == 0 and interaction.target_momentum[2] == 0 and interaction.target_momentum[3] == 0) {
+        p3 = p3_lab;
+        p4 = p4_lab;
+    } else {
+        rk::Boost boost_lab_to_start = p2.labBoost();
+        p3 = boost_lab_to_start * p3_lab;
+        p4 = boost_lab_to_start * p4_lab;
+    }
+
+    // TODO: helicity update for secondary particles
+    interaction.secondary_momenta.resize(2);
+
+    interaction.secondary_momenta[0][0] = p3.e(); // p3_energy
+    interaction.secondary_momenta[0][1] = p3.px(); // p3_x
+    interaction.secondary_momenta[0][2] = p3.py(); // p3_y
+    interaction.secondary_momenta[0][3] = p3.pz(); // p3_z
+
+    interaction.secondary_momenta[0][0] = p4.e(); // p4_energy
+    interaction.secondary_momenta[0][1] = p4.px(); // p4_x
+    interaction.secondary_momenta[0][2] = p4.py(); // p4_y
+    interaction.secondary_momenta[0][3] = p4.pz(); // p4_z
+    interaction.secondary_masses[0] = p4.m();
+
 }
 
 } // namespace crosssections
