118f49846058eb0780daa7320cd0c562b0e00f7c
Austin Schneider <hogenshpogen@gmail.com>
2024-01-10 13:08:29 -0700

    Remove unused variable

--- projects/geometry/private/test/Geometry_TEST.cxx
+++ projects/geometry/private/test/Geometry_TEST.cxx
@@ -158,7 +158,7 @@ TEST(IsInside, Box)
     Vector3D position_geometry(0, 0, 0);
 
     int is_inside  = 0;
-    int is_outside = 0;
+    // int is_outside = 0;
 
     double volumia_ratio = 0;
 
@@ -215,13 +215,13 @@ TEST(IsInside, Box)
                 EXPECT_TRUE(A.IsInside(particle_position, particle_direction));
             } else
             {
-                is_outside++;
+                // is_outside++;
                 EXPECT_FALSE(A.IsInside(particle_position, particle_direction));
             }
         }
         ASSERT_NEAR(1. * is_inside, volumia_ratio * number_particles, 3 * sqrt(volumia_ratio * number_particles));
         is_inside  = 0;
-        is_outside = 0;
+        // is_outside = 0;
     }
     // Check what happens if particles are on the border of the box
 
@@ -383,7 +383,7 @@ TEST(IsInside, Cylinder)
     Vector3D position_geometry(0, 0, 0);
 
     int is_inside  = 0;
-    int is_outside = 0;
+    // int is_outside = 0;
 
     double volumia_ratio = 0;
 
@@ -449,13 +449,13 @@ TEST(IsInside, Cylinder)
                 EXPECT_TRUE(A.IsInside(particle_position, particle_direction));
             } else
             {
-                is_outside++;
+                // is_outside++;
                 EXPECT_FALSE(A.IsInside(particle_position, particle_direction));
             }
         }
         ASSERT_NEAR(1. * is_inside, volumia_ratio * number_particles, 3 * sqrt(volumia_ratio * number_particles));
         is_inside  = 0;
-        is_outside = 0;
+        // is_outside = 0;
     }
 
     // Test borders
@@ -596,7 +596,7 @@ TEST(IsInside, Sphere)
     Vector3D position_geometry(0, 0, 0);
 
     int is_inside  = 0;
-    int is_outside = 0;
+    // int is_outside = 0;
 
     double volumia_ratio = 0;
 
@@ -653,13 +653,13 @@ TEST(IsInside, Sphere)
                 EXPECT_TRUE(A.IsInside(particle_position, particle_direction));
             } else
             {
-                is_outside++;
+                // is_outside++;
                 EXPECT_FALSE(A.IsInside(particle_position, particle_direction));
             }
         }
         ASSERT_NEAR(1. * is_inside, volumia_ratio * number_particles, 3 * sqrt(volumia_ratio * number_particles));
         is_inside  = 0;
-        is_outside = 0;
+        // is_outside = 0;
     }
 
     // Test borders
