9950257a57659b52ab858c0f40e596887a7b3ff0
Austin Schneider <hogenshpogen@gmail.com>
2024-01-21 18:05:20 -0700

    Get IceCube example working

--- resources/Examples/Example1/DIS_IceCube.py
+++ resources/Examples/Example1/DIS_IceCube.py
@@ -3,64 +3,67 @@ import os
 
 import leptoninjector as LI
 import sys
-sys.path.insert(1,'/n/holylfs05/LABS/arguelles_delgado_lab/Everyone/nkamp/LIV2/sources/LeptonInjector/python')
-from LIController import LIController
- 
-LI_SRC = os.environ.get('LEPTONINJECTOR_SRC')
+from leptoninjector import _util
+from leptoninjector.LIController import LIController
+
+resources_dir = _util.resource_package_dir()
 
 # Number of events to inject
 events_to_inject = 1000
 
 # Expeirment to run
-experiment = 'IceCube'
+experiment = "IceCube"
 
 # Define the controller
-controller = LIController(events_to_inject,
-                          experiment)
+controller = LIController(events_to_inject, experiment)
 
 # Particle to inject
 primary_type = LI.dataclasses.Particle.ParticleType.NuMu
 
 # Cross Section Model
-xsfiledir = LI_SRC+'resources/CrossSectionTables/DISSplines/'
+xsfiledir = os.path.join(resources_dir, "CrossSectionTables", "DISSplines")
 target_type = LI.dataclasses.Particle.ParticleType.Nucleon
 
-DIS_xs = LI.interactions.DISFromSpline(xsfiledir+'test_xs.fits',
-                                        xsfiledir+'test_xs_total.fits',
-                                        [primary_type],
-                                        [target_type])
+DIS_xs = LI.interactions.DISFromSpline(
+    os.path.join(xsfiledir, "test_xs.fits"),
+    os.path.join(xsfiledir, "test_xs_total.fits"),
+    [primary_type],
+    [target_type],
+)
 
-primary_xs = LI.interactions.CrossSectionCollection(primary_type, [DIS_xs])
-controller.SetCrossSections(primary_xs)
+primary_xs = LI.interactions.InteractionCollection(primary_type, [DIS_xs])
+controller.SetInteractions(primary_xs)
 
 # Primary distributions
 primary_injection_distributions = {}
 primary_physical_distributions = {}
 
 # energy distribution
-edist = LI.distributions.PowerLaw(2,1e3,1e6)
-primary_injection_distributions['energy'] = edist
-primary_physical_distributions['energy'] = edist
+edist = LI.distributions.PowerLaw(2, 1e3, 1e6)
+primary_injection_distributions["energy"] = edist
+primary_physical_distributions["energy"] = edist
 
 # direction distribution
 direction_distribution = LI.distributions.IsotropicDirection()
-primary_injection_distributions['direction'] = direction_distribution
-primary_physical_distributions['direction'] = direction_distribution
+primary_injection_distributions["direction"] = direction_distribution
+primary_physical_distributions["direction"] = direction_distribution
 
 # position distribution
 muon_range_func = LI.distributions.LeptonDepthFunction()
-position_distribution = LI.distributions.ColumnDepthPositionDistribution(600, 600.,
-                                                                         muon_range_func,
-                                                                         set(controller.GetEarthModelTargets()[0]))
-primary_injection_distributions['position'] = position_distribution
+position_distribution = LI.distributions.ColumnDepthPositionDistribution(
+    600, 600.0, muon_range_func, set(controller.GetDetectorModelTargets()[0])
+)
+primary_injection_distributions["position"] = position_distribution
 
 # SetProcesses
-controller.SetProcesses(primary_type,
-                        primary_injection_distributions,
-                        primary_physical_distributions)
+controller.SetProcesses(
+    primary_type, primary_injection_distributions, primary_physical_distributions
+)
 
 controller.Initialize()
 
 events = controller.GenerateEvents()
 
-controller.SaveEvents('output/MINERvA_Dipole_M%2.2f_mu%2.2e_example.hdf5'%(model_kwargs['m4'],model_kwargs['mu_tr_mu4']))
+controller.SaveEvents(
+    "IceCube_DIS.hdf5"
+)
