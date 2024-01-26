16dd57a201c1076371d36401f32294ca593011a7
Austin Schneider <hogenshpogen@gmail.com>
2024-01-25 20:53:54 -0700

    Remove comment

--- projects/dataclasses/private/InteractionSignature.cxx
+++ projects/dataclasses/private/InteractionSignature.cxx
@@ -11,31 +11,6 @@ bool InteractionSignature::operator==(InteractionSignature const & other) const 
         std::tie(primary_type, target_type, secondary_types)
         ==
         std::tie(other.primary_type, other.target_type, other.secondary_types);
-    /*
-    if(primary_type != other.primary_type or target_type != other.target_type) {
-        return false;
-    } else {
-        std::map<LI::dataclasses::Particle::ParticleType, int> m0;
-        for(auto p : secondary_types) {
-            auto it = m0.find(p);
-            if(it == m0.end()) {
-                m0.insert({p, 1});
-            } else {
-                it->second += 1;
-            }
-        }
-        std::map<LI::dataclasses::Particle::ParticleType, int> m1;
-        for(auto p : other.secondary_types) {
-            auto it = m1.find(p);
-            if(it == m1.end()) {
-                m1.insert({p, 1});
-            } else {
-                it->second += 1;
-            }
-        }
-        return m0 == m1;
-    }
-    */
 }
 
 bool InteractionSignature::operator<(InteractionSignature const & other) const {
