98f04928c5a42fe1283334ebf13ad3cac86e4120
Austin Schneider <hogenshpogen@gmail.com>
2024-01-31 13:34:25 -0700

    ParticleID

---
+++ projects/dataclasses/private/ParticleID.cxx
@@ -0,0 +1,66 @@
+#include "LeptonInjector/dataclasses/ParticleID.h"
+
+#include <tuple>
+#include <atomic>
+#include <unistd.h>
+
+namespace LI {
+namespace dataclasses {
+
+namespace {
+	std::mutex global_id_lock;
+	std::atomic<int32_t> global_last_pid_(0);
+	std::atomic<int32_t> global_minor_id_(0);
+	std::atomic<uint64_t> global_major_id_(0);
+}
+
+bool ParticleID::operator<(ParticleID const & other) const {
+    return std::tie(major_id, minor_id) < std::tie(other.major_id, other.minor_id);
+}
+
+bool ParticleID::operator==(ParticleID const & other) const {
+    return std::tie(major_id, minor_id) == std::tie(other.major_id, other.minor_id);
+}
+
+// Adapted from https://github.com/icecube/icetray-public/blob/4436c3e10c23f95a8965c98fecccb7775a361fab/dataclasses/private/dataclasses/physics/I3Particle.cxx#L42-L93
+ParticleID ParticleID::GenerateID() {
+    int this_pid = getpid();
+
+    int32_t last_pid = global_last_pid_.load(std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_acquire); //keep memory ops from wandering
+    if (this_pid != last_pid) {
+        std::lock_guard<std::mutex> lg(global_id_lock); //acquire the lock
+                                                            //check whether another thread already updated this
+        last_pid = global_last_pid_.load(std::memory_order_relaxed);
+        if(this_pid != last_pid){
+            std::atomic_thread_fence(std::memory_order_release);
+            global_last_pid_.store(this_pid, std::memory_order_relaxed);
+            global_major_id_.store(0, std::memory_order_relaxed); // this will cause a new major ID to be generated
+            global_minor_id_.store(0, std::memory_order_relaxed); // reset the minor ID, too
+        }
+    }
+
+    uint64_t old_major_id = global_major_id_.load(std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_acquire); //keep memory ops from wandering
+    if(old_major_id == 0){
+        std::lock_guard<std::mutex> lg(global_id_lock); //acquire the lock
+        //check whether another thread already updated this
+        old_major_id = global_major_id_.load(std::memory_order_relaxed);
+        if(old_major_id == 0){
+            std::hash<std::string> string_hash;
+            std::stringstream s;
+            s << time(0) << this_pid<<gethostid();
+            std::atomic_thread_fence(std::memory_order_release);
+            global_major_id_.store(string_hash(s.str()), std::memory_order_relaxed);
+        }
+    }
+    ParticleID ID;
+
+    ID.major_id = global_major_id_.load();
+    ID.minor_id = global_minor_id_.fetch_add(1);
+
+    return ID;
+}
+
+} // namespace dataclasses
+} // namespace LI
---
+++ projects/dataclasses/public/LeptonInjector/dataclasses/ParticleID.h
@@ -0,0 +1,54 @@
+#ifndef LI_ParticleID_H
+#define LI_ParticleID_H
+
+#include <array>                                        // for array
+#include <vector>                                       // for vector
+#include <cstdint>                                      // for uint32_t
+#include <ostream>                                      // for ostream
+#include <stdexcept>                                    // for runtime_error
+
+#include <cereal/cereal.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/utility.hpp>
+
+#include "LeptonInjector/dataclasses/DecaySignature.h"
+
+namespace LI {
+namespace dataclasses {
+
+struct ParticleID {
+    uint64_t major_id;
+    int64_t minor_id;
+
+    ParticleID() : major_id(0), minor_id(0){};
+    ParticleID(uint64_t major, int32_t minor) : 
+        major_id(major), minor_id(minor) {};
+
+    static ParticleID GenerateID();
+
+    bool operator<(ParticleID const & other) const;
+
+    bool operator==(ParticleID const & other) const;
+    friend std::ostream& operator<<(std::ostream& os, ParticleID const& record);
+    template<class Archive>
+    void serialize(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(::cereal::make_nvp("MajorID", major_id));
+            archive(::cereal::make_nvp("MinorID", minor_id));
+        } else {
+            throw std::runtime_error("ParticleID only supports version <= 0!");
+        }
+    };
+};
+
+} // namespace dataclasses
+} // namespace LI
+
+CEREAL_CLASS_VERSION(LI::dataclasses::ParticleID, 0);
+
+#endif // LI_ParticleID_H
