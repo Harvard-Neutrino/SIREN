65b93a117563b24557312e639abdb3d2a6f093fc
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-02-07 14:51:24 -0500

    clean up flux directories, add tools to compute fluxes on the fly

--- resources/Fluxes/BNB_Flux_Tables/make_MB_flux_tables.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-fluxTotal = np.loadtxt('mbflux.txt')
-Emid = 0.5*(fluxTotal[:,0]+fluxTotal[:,1])
-bnb_flux = {}
-for i,key in enumerate(['numu','numubar','nue','nuebar']):
-	bnb_flux[key] = np.zeros((len(Emid),2))
-	bnb_flux[key][:,0] = Emid
-	bnb_flux[key][:,1] = fluxTotal[:,i+2]*(1000./50.)
-	np.savetxt('BNB_%s_flux.txt'%key,bnb_flux[key])
