c19596f7c57ad851129605a6c1a0a198a7db556e
Austin Schneider <hogenshpogen@gmail.com>
2024-06-06 09:47:15 -0500

    Introspect package version in __init__.py

--- python/__init__.py
+++ python/__init__.py
@@ -9,6 +9,16 @@ from . import injection
 
 from . import _util
 
+# Intropspect package version
+import sys
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+__version__ = metadata.version(__package__)
+del sys
+del metadata
+
 # set up some public-facing utilities functions
 utilities.get_resource_package_dir = _util.resource_package_dir
 utilities.get_detector_model_path = _util.get_detector_model_path
