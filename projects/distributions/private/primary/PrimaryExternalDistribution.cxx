65fba94cd11669a9f8d2de589d2dc9d520ccd4d9
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-07-29 17:38:00 -0400

    first attempt at external list primary injector

---
+++ projects/distributions/private/primary/PrimaryExternalDistribution.cxx
@@ -0,0 +1,131 @@
+#include "SIREN/distributions/primary/PrimaryExternalDistribution.h"
+
+#include <array>                                           // for array
+#include <string>                                          // for basic_string
+
+#include "SIREN/dataclasses/InteractionRecord.h"  // for Interactio...
+#include "SIREN/utilities/Random.h"               // for SIREN_random
+
+namespace siren {
+namespace distributions {
+
+//---------------
+// class PrimaryExternalDistribution : PrimaryExternalDistribution
+//---------------
+PrimaryExternalDistribution::PrimaryExternalDistribution(std::string filename)
+{
+    input_file.open(filename);
+    init_pos_set = false;
+    mom_set = false;
+
+    std::string line;
+    if (!input_file.is_open()) {
+        std::cerr << "error: file open failed " << filename << ".\n";
+        exit(0);
+    }
+    std::getline(input_file,line);
+
+    std::stringstream ss(line);
+    std::string key;
+    while (std::getline(ss, key, ',')) {
+        keys.push_back(key);
+        if (key == "x0" ||
+            key == "y0" ||
+            key == "z0") init_pos_set = true;
+        if (key == "px" ||
+            key == "py" ||
+            key == "pz") mom_set = true;
+    }
+
+    std::string value;
+    // fill input data
+    while (std::getline(input_file,line)) {
+        std::vector<double> tmp_data;
+        ss.str(line);
+        while (std::getline(ss, value, ',')) {
+            tmp_data.push_back(stod(value));
+        }
+        input_data.push_back(tmp_data);
+    }
+
+}
+
+void PrimaryExternalDistribution::Sample(
+        std::shared_ptr<siren::utilities::SIREN_random> rand,
+        std::shared_ptr<siren::detector::DetectorModel const> detector_model,
+        std::shared_ptr<siren::interactions::InteractionCollection const> interactions,
+        siren::dataclasses::PrimaryDistributionRecord & record) const {
+
+    int i = int(rand->Uniform() * input_data.size());
+    int i_key = 0;
+    std::array<double, 3> _initial_position;
+    std::array<double, 3> _momentum;
+    for (auto value : input_data[i]) {
+        if (keys[i_key] == "x0") {
+            _initial_position[0] = value;
+        }
+        else if (keys[i_key] == "y0") {
+            _initial_position[1] = value;
+        }
+        else if (keys[i_key] == "z0") {
+            _initial_position[2] = value;
+        }
+        else if (keys[i_key] == "px") {
+            _momentum[0] = value;
+        }
+        else if (keys[i_key] == "py") {
+            _momentum[1] = value;
+        }
+        else if (keys[i_key] == "pz") {
+            _momentum[2] = value;
+        }
+        else if (keys[i_key] == "E") {
+            record.SetEnergy(value);
+        }
+        else if (keys[i_key] == "m") {
+            record.SetMass(value);
+        }
+        else {
+            record.SetInteractionParameter(keys[i_key],value);
+        }
+        ++i_key;
+    }
+    if(mom_set) record.SetThreeMomentum(_momentum);
+    if(init_pos_set) record.SetInitialPosition(_initial_position);
+}
+
+std::vector<std::string> PrimaryExternalDistribution::DensityVariables() const {
+    return std::vector<std::string>{"External"};
+}
+
+std::string PrimaryExternalDistribution::Name() const {
+    return "PrimaryExternalDistribution";
+}
+
+double PrimaryExternalDistribution::GenerationProbability(std::shared_ptr<siren::detector::DetectorModel const> detector_model,
+                                                          std::shared_ptr<siren::interactions::InteractionCollection const> interactions,
+                                                          siren::dataclasses::InteractionRecord const & record) const {
+    return 1;
+}
+
+std::shared_ptr<PrimaryInjectionDistribution> PrimaryExternalDistribution::clone() const {
+    return std::shared_ptr<PrimaryInjectionDistribution>(new PrimaryExternalDistribution(*this));
+}
+
+bool PrimaryExternalDistribution::equal(WeightableDistribution const & other) const {
+    const PrimaryExternalDistribution* x = dynamic_cast<const PrimaryExternalDistribution*>(&other);
+
+    if(!x)
+        return false;
+    else
+        return input_file == other->input_file;
+}
+
+bool PrimaryExternalDistribution::less(WeightableDistribution const & other) const {
+    const PrimaryExternalDistribution* x = dynamic_cast<const PrimaryExternalDistribution*>(&other);
+    return input_file != other->input_file;
+}
+
+
+} // namespace distributions
+} // namespace sirenREN
---
+++ projects/distributions/public/SIREN/distributions/primary/PrimaryExternalDistribution.h
@@ -0,0 +1,77 @@
+#pragma once
+#ifndef SIREN_PrimaryExternalDistribution_H
+#define SIREN_PrimaryExternalDistribution_H
+
+#include <memory>                                        // for shared_ptr
+#include <string>                                        // for string
+#include <vector>                                        // for vector
+#include <cstdint>                                       // for uint32_t
+#include <stdexcept>                                     // for runtime_error
+
+#include <cereal/access.hpp>
+#include <cereal/types/polymorphic.hpp>
+#include <cereal/types/base_class.hpp>
+#include <cereal/types/utility.hpp>
+
+#include "SIREN/distributions/Distributions.h"  // for WeightableDi...
+
+namespace siren { namespace interactions { class InteractionCollection; } }
+namespace siren { namespace dataclasses { class InteractionRecord; } }
+namespace siren { namespace detector { class DetectorModel; } }
+namespace siren { namespace utilities { class SIREN_random; } }
+namespace cereal { class access; }
+
+namespace siren {
+namespace distributions {
+
+class PrimaryExternalDistribution : virtual public PrimaryInjectionDistribution {
+friend cereal::access;
+public:
+    ~PrimaryExternalDistribution() {
+        input_file.close();
+    };
+private:
+    std::vector<std::vector<double>> input_data;
+    std::ifstream input_file;
+    std::vector<std::string> keys;
+    std::vector<std::string> possible_keys = {"x0","y0","z0","m","E","px","py","pz"};
+    bool init_pos_set;
+    bool mom_set;
+public:
+    PrimaryExternalDistribution(std::string input_file);
+    void Sample(std::shared_ptr<siren::utilities::SIREN_random> rand, std::shared_ptr<siren::detector::DetectorModel const> detector_model, std::shared_ptr<siren::interactions::InteractionCollection const> interactions, siren::dataclasses::PrimaryDistributionRecord & record) const override;
+    virtual double GenerationProbability(std::shared_ptr<siren::detector::DetectorModel const> detector_model, std::shared_ptr<siren::interactions::InteractionCollection const> interactions, siren::dataclasses::InteractionRecord const & record) const override;
+    virtual std::vector<std::string> DensityVariables() const override;
+    virtual std::string Name() const override;
+    virtual std::shared_ptr<PrimaryInjectionDistribution> clone() const override;
+    template<typename Archive>
+    void save(Archive & archive, std::uint32_t const version) const {
+        if(version == 0) {
+            archive(cereal::virtual_base_class<PrimaryInjectionDistribution>(this));
+            archive(cereal::virtual_base_class<PhysicallyNormalizedDistribution>(this));
+        } else {
+            throw std::runtime_error("PrimaryExternalDistribution only supports version <= 0!");
+        }
+    }
+    template<typename Archive>
+    void load(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(cereal::virtual_base_class<PrimaryInjectionDistribution>(this));
+            archive(cereal::virtual_base_class<PhysicallyNormalizedDistribution>(this));
+        } else {
+            throw std::runtime_error("PrimaryExternalDistribution only supports version <= 0!");
+        }
+    }
+protected:
+    virtual bool equal(WeightableDistribution const & distribution) const override;
+    virtual bool less(WeightableDistribution const & distribution) const override;
+};
+
+} // namespace distributions
+} // namespace siren
+
+CEREAL_CLASS_VERSION(siren::distributions::PrimaryExternalDistribution, 0);
+CEREAL_REGISTER_TYPE(siren::distributions::PrimaryExternalDistribution);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(siren::distributions::PrimaryInjectionDistribution, siren::distributions::PrimaryExternalDistribution);
+
+#endif // SIREN_PrimaryExternalDistribution_H
