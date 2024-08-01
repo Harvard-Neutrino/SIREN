bf1307758c3ad42e0236886c74687b920ba4e424
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-08-01 19:05:33 -0400

    introduce primary bounded and physical vertex distributions to sample vertex in the case that the inital position is already provided from the external distribution

---
+++ projects/distributions/private/primary/vertex/PrimaryPhysicalVertexDistribution.cxx
@@ -0,0 +1,183 @@
+#include "SIREN/distributions/primary/vertex/PrimaryPhysicalVertexDistribution.h"
+
+#include <set>                                                    // for set
+#include <array>                                                  // for array
+#include <cmath>                                                  // for exp
+#include <tuple>                                                  // for tie
+#include <string>                                                 // for bas...
+#include <vector>                                                 // for vector
+
+#include "SIREN/interactions/CrossSection.h"            // for Cro...
+#include "SIREN/interactions/InteractionCollection.h"  // for Cro...
+#include "SIREN/dataclasses/InteractionRecord.h"         // for Int...
+#include "SIREN/dataclasses/InteractionSignature.h"      // for Int...
+#include "SIREN/dataclasses/Particle.h"                  // for Par...
+#include "SIREN/detector/DetectorModel.h"                   // for Ear...
+#include "SIREN/detector/Path.h"                         // for Path
+#include "SIREN/detector/Coordinates.h"
+#include "SIREN/distributions/Distributions.h"           // for Inj...
+#include "SIREN/geometry/Geometry.h"                     // for Geo...
+#include "SIREN/math/Vector3D.h"                         // for Vec...
+#include "SIREN/utilities/Errors.h"                      // for Sec...
+#include "SIREN/utilities/Random.h"                      // for SIREN_...
+
+namespace siren {
+namespace distributions {
+
+using detector::DetectorPosition;
+using detector::DetectorDirection;
+
+namespace {
+double log_one_minus_exp_of_negative(double x) {
+    if(x < 1e-1) {
+        return std::log(x) - x/2.0 + x*x/24.0 - x*x*x*x/2880.0;
+    } else if(x > 3) {
+        double ex = std::exp(-x);
+        double ex2 = ex * ex;
+        double ex3 = ex2 * ex;
+        double ex4 = ex3 * ex;
+        double ex5 = ex4 * ex;
+        double ex6 = ex5 * ex;
+        return -(ex + ex2 / 2.0 + ex3 / 3.0 + ex4 / 4.0 + ex5 / 5.0 + ex6 / 6.0);
+    } else {
+        return std::log(1.0 - std::exp(-x));
+    }
+}
+}
+
+//---------------
+// class PrimaryPhysicalVertexDistribution : public VertexPositionDistribution
+//---------------
+
+
+std::tuple<siren::math::Vector3D, siren::math::Vector3D> PrimaryPhysicalVertexDistribution::SamplePosition(std::shared_ptr<siren::utilities::SIREN_random> rand, std::shared_ptr<siren::detector::DetectorModel const> detector_model, std::shared_ptr<siren::interactions::InteractionCollection const> interactions, siren::dataclasses::PrimaryDistributionRecord & record) const {
+    siren::math::Vector3D pos = record.GetInitialPosition();
+    siren::math::Vector3D dir = record.GetDirection();
+
+    siren::math::Vector3D endcap_0 = pos;
+
+    siren::detector::Path path(detector_model, DetectorPosition(endcap_0), DetectorDirection(dir), std::numeric_limits<double>::infinity());
+    path.ClipToOuterBounds();
+
+    std::vector<siren::dataclasses::ParticleType> targets(interactions->TargetTypes().begin(), interactions->TargetTypes().end());
+
+    siren::dataclasses::InteractionRecord fake_record;
+    record.Finalize(fake_record);
+    std::vector<double> total_cross_sections(targets.size(), 0.0);
+    double total_decay_length = interactions->TotalDecayLength(fake_record);
+    for(unsigned int i=0; i<targets.size(); ++i) {
+        siren::dataclasses::ParticleType const & target = targets[i];
+        fake_record.signature.target_type = target;
+        fake_record.target_mass = detector_model->GetTargetMass(target);
+        for(auto const & cross_section : interactions->GetCrossSectionsForTarget(target)) {
+            total_cross_sections[i] += cross_section->TotalCrossSectionAllFinalStates(fake_record);
+        }
+    }
+
+    double total_interaction_depth = path.GetInteractionDepthInBounds(targets, total_cross_sections, total_decay_length);
+    if(total_interaction_depth == 0) {
+        throw(siren::utilities::InjectionFailure("No available interactions along path!"));
+    }
+
+    double traversed_interaction_depth;
+    if(total_interaction_depth < 1e-6) {
+        traversed_interaction_depth = rand->Uniform() * total_interaction_depth;
+    } else {
+        double exp_m_total_interaction_depth = exp(-total_interaction_depth);
+
+        double y = rand->Uniform();
+        traversed_interaction_depth = -log(y * exp_m_total_interaction_depth + (1.0 - y));
+    }
+
+    double dist = path.GetDistanceFromStartAlongPath(traversed_interaction_depth, targets, total_cross_sections, total_decay_length);
+    siren::math::Vector3D vertex = path.GetFirstPoint() + dist * path.GetDirection();
+
+    return {pos, vertex};
+}
+
+double PrimaryPhysicalVertexDistribution::GenerationProbability(std::shared_ptr<siren::detector::DetectorModel const> detector_model, std::shared_ptr<siren::interactions::InteractionCollection const> interactions, siren::dataclasses::InteractionRecord const & record) const {
+    siren::math::Vector3D dir(record.primary_momentum[1], record.primary_momentum[2], record.primary_momentum[3]);
+    dir.normalize();
+    siren::math::Vector3D vertex(record.interaction_vertex);
+
+    siren::math::Vector3D endcap_0 = record.primary_initial_position;
+
+    siren::detector::Path path(detector_model, DetectorPosition(endcap_0), DetectorDirection(dir), std::numeric_limits<double>::infinity());
+    path.ClipToOuterBounds();
+
+    if(not path.IsWithinBounds(DetectorPosition(vertex)))
+        return 0.0;
+
+    std::set<siren::dataclasses::ParticleType> const & possible_targets = interactions->TargetTypes();
+
+    std::vector<siren::dataclasses::ParticleType> targets(possible_targets.begin(), possible_targets.end());
+    std::vector<double> total_cross_sections(targets.size(), 0.0);
+    double total_decay_length = interactions->TotalDecayLength(record);
+    siren::dataclasses::InteractionRecord fake_record = record;
+    for(unsigned int i=0; i<targets.size(); ++i) {
+        siren::dataclasses::ParticleType const & target = targets[i];
+        fake_record.signature.target_type = target;
+        fake_record.target_mass = detector_model->GetTargetMass(target);
+        for(auto const & cross_section : interactions->GetCrossSectionsForTarget(target)) {
+            total_cross_sections[i] += cross_section->TotalCrossSectionAllFinalStates(fake_record);
+        }
+    }
+    double total_interaction_depth = path.GetInteractionDepthInBounds(targets, total_cross_sections, total_decay_length);
+
+    path.SetPointsWithRay(path.GetFirstPoint(), path.GetDirection(), path.GetDistanceFromStartInBounds(DetectorPosition(vertex)));
+
+    double traversed_interaction_depth = path.GetInteractionDepthInBounds(targets, total_cross_sections, total_decay_length);
+
+    double interaction_density = detector_model->GetInteractionDensity(path.GetIntersections(), DetectorPosition(vertex), targets, total_cross_sections, total_decay_length);
+
+    double prob_density;
+    if(total_interaction_depth < 1e-6) {
+        prob_density = interaction_density / total_interaction_depth;
+    } else {
+        prob_density = interaction_density * exp(-log_one_minus_exp_of_negative(total_interaction_depth) - traversed_interaction_depth);
+    }
+
+    return prob_density;
+}
+
+PrimaryPhysicalVertexDistribution::PrimaryPhysicalVertexDistribution() {}
+
+std::string PrimaryPhysicalVertexDistribution::Name() const {
+    return "PrimaryPhysicalVertexDistribution";
+}
+
+std::shared_ptr<PrimaryInjectionDistribution> PrimaryPhysicalVertexDistribution::clone() const {
+    return std::shared_ptr<PrimaryInjectionDistribution>(new PrimaryPhysicalVertexDistribution(*this));
+}
+
+std::tuple<siren::math::Vector3D, siren::math::Vector3D> PrimaryPhysicalVertexDistribution::InjectionBounds(std::shared_ptr<siren::detector::DetectorModel const> detector_model, std::shared_ptr<siren::interactions::InteractionCollection const> interactions, siren::dataclasses::InteractionRecord const & record) const {
+    siren::math::Vector3D dir(record.primary_momentum[1], record.primary_momentum[2], record.primary_momentum[3]);
+    dir.normalize();
+    siren::math::Vector3D vertex(record.interaction_vertex);
+
+    siren::math::Vector3D endcap_0 = record.primary_initial_position;
+
+    siren::detector::Path path(detector_model, DetectorPosition(endcap_0), DetectorDirection(dir), std::numeric_limits<double>::infinity());
+    path.ClipToOuterBounds();
+
+    if(not path.IsWithinBounds(DetectorPosition(vertex)))
+        return std::tuple<siren::math::Vector3D, siren::math::Vector3D>(siren::math::Vector3D(0, 0, 0), siren::math::Vector3D(0, 0, 0));
+    return std::tuple<siren::math::Vector3D, siren::math::Vector3D>(path.GetFirstPoint(), path.GetLastPoint());
+}
+
+bool PrimaryPhysicalVertexDistribution::equal(WeightableDistribution const & other) const {
+    const PrimaryPhysicalVertexDistribution* x = dynamic_cast<const PrimaryPhysicalVertexDistribution*>(&other);
+
+    if(!x)
+        return false;
+    else
+        return true;
+}
+
+bool PrimaryPhysicalVertexDistribution::less(WeightableDistribution const & other) const {
+    const PrimaryPhysicalVertexDistribution* x = dynamic_cast<const PrimaryPhysicalVertexDistribution*>(&other);
+    return false;
+}
+
+} // namespace distributions
+} // namespace sirenREN
