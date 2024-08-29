811780d2f66188e0dc1188e38dfea8340c6b43bd
Austin Schneider <hogenshpogen@gmail.com>
2024-08-28 23:12:12 -0600

    Check for DarkNews.ModelContainer.configure_logger in case it is removed in the future.

--- resources/Processes/DarkNewsTables/logger.py
+++ resources/Processes/DarkNewsTables/logger.py
@@ -1,12 +1,18 @@
 # Monkey patch DarkNews logger to hide printouts
 import functools
 
-from DarkNews.ModelContainer import ModelContainer
-ModelContainer_configure_logger = ModelContainer.configure_logger
+dn_has_modelcontainer_logger = False
+try:
+    from DarkNews.ModelContainer import ModelContainer
+    ModelContainer_configure_logger = ModelContainer.configure_logger
+    dn_has_modelcontainer_logger = True
+except:
+    pass
 
-@functools.wraps(ModelContainer.configure_logger)
-def suppress_info(self, logger, loglevel="INFO", prettyprinter=None, logfile=None, verbose=False):
-    return ModelContainer_configure_logger(self, logger, loglevel="WARNING", prettyprinter=prettyprinter, logfile=logfile, verbose=verbose)
+if dn_has_modelcontainer_logger:
+    @functools.wraps(ModelContainer.configure_logger)
+    def suppress_info(self, logger, loglevel="INFO", prettyprinter=None, logfile=None, verbose=False):
+        return ModelContainer_configure_logger(self, logger, loglevel="WARNING", prettyprinter=prettyprinter, logfile=logfile, verbose=verbose)
 
-ModelContainer.configure_logger = suppress_info
+    ModelContainer.configure_logger = suppress_info
 
