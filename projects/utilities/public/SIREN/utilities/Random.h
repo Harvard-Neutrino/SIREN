dc45403a43af7ad592ef865f21aa1959b23a4293
Austin Schneider <hogenshpogen@gmail.com>
2024-09-11 22:24:13 -0600

    Produce a random seed by default. Getter for seed

--- projects/utilities/public/SIREN/utilities/Random.h
+++ projects/utilities/public/SIREN/utilities/Random.h
@@ -8,6 +8,7 @@
 // this implements a class to sample numbers just like in an i3 service
 
 #include <random> // default_random_engine, uniform_real_distribution
+#include <cstdint>                                      // for uint32_t
 
 #include <cereal/cereal.hpp>
 #include <cereal/archives/json.hpp>
@@ -22,42 +23,45 @@
 namespace siren {
 namespace utilities {
 
-    class SIREN_random{
-        public:
-            SIREN_random();
-            SIREN_random( unsigned int _seed );
+class SIREN_random{
+public:
+    SIREN_random();
+    SIREN_random(uint64_t _seed);
 
-            // this naming convention is used to
-            double Uniform( double from=0.0, double to=1.0);
-            double PowerLaw(double min, double max, double n); 
+    // this naming convention is used to
+    double Uniform( double from=0.0, double to=1.0);
+    double PowerLaw(double min, double max, double n);
 
-            // in case this is set up without a seed!
-            void set_seed(unsigned int new_seed);
+    // in case this is set up without a seed!
+    void set_seed(uint64_t new_seed);
+    uint64_t get_seed() const;
 
-            template<class Archive>
-            void save(Archive & archive, std::uint32_t const version) const {
-            if(version == 0) {
-                archive(::cereal::make_nvp("Seed", seed));
-            } else {
-                throw std::runtime_error("SIREN_random only supports version <= 0!");
-            }
-            };
+    static uint64_t generate_seed();
 
-            template<class Archive>
-            void load(Archive & archive, std::uint32_t const version) {
-            if(version == 0) {
-                archive(::cereal::make_nvp("Seed", seed));
-                set_seed(seed);
-            } else {
-                throw std::runtime_error("SIREN_random only supports version <= 0!");
-            }
-            };
-
-        private:
-            unsigned int seed;
-            std::default_random_engine configuration;
-            std::uniform_real_distribution<double> generator;
+    template<class Archive>
+    void save(Archive & archive, std::uint32_t const version) const {
+        if(version == 0) {
+            archive(::cereal::make_nvp("Seed", seed));
+        } else {
+            throw std::runtime_error("SIREN_random only supports version <= 0!");
+        }
     };
+
+    template<class Archive>
+    void load(Archive & archive, std::uint32_t const version) {
+        if(version == 0) {
+            archive(::cereal::make_nvp("Seed", seed));
+            set_seed(seed);
+        } else {
+            throw std::runtime_error("SIREN_random only supports version <= 0!");
+        }
+    };
+
+private:
+    uint64_t seed;
+    std::default_random_engine configuration;
+    std::uniform_real_distribution<double> generator;
+};
 
 } // namespace utilities
 } // namespace siren
