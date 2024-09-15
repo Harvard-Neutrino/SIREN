0d41fb18574a9d3f5ddc887edf075fb5b57d4a1b
Austin Schneider <hogenshpogen@gmail.com>
2024-09-15 17:44:58 -0600

    Rename and fix T2K_NEAR flux script to work with new directory structure.

--- resources/fluxes/T2K_NEAR/T2K_NEAR-v1.0/flux.py
+++ resources/fluxes/T2K_NEAR/T2K_NEAR-v1.0/flux.py
@@ -1,24 +1,26 @@
 import os
 
-def MakeFluxFile(tag, abs_flux_dir):
-    
+def load_flux(tag):
+
     '''
     Accepts the following tags:
         {PLUS, MINUS}_{nue,nuebar,numu,numubar}
     '''
 
     enhance, particle = tag.split("_")
-    
+
     if enhance not in ["MINUS", "PLUS"]:
         print("%s 250kA enhancement specified in tag %s is not valid"%(enhance))
         exit(0)
     if particle not in ["numu", "numubar", "nue", "nuebar"]:
         print("%s particle specified in tag %s is not valid"%(particle))
         exit(0)
-        
+
+    abs_flux_dir = os.path.dirname(__file__)
+
     input_flux_file = os.path.join(abs_flux_dir,
                                    "T2K_%s_250kA.dat"%(enhance))
-    
+
     output_flux_file = os.path.join(abs_flux_dir,
                                     "T2KOUT_%s.dat"%(tag))
 
@@ -32,4 +34,4 @@ def MakeFluxFile(tag, abs_flux_dir):
                 E, flux = (float(row[1])+float(row[3]))/2, float(row[pid+2])
                 flux*=2e-16 # put flux in units of nu/m^2/GeV/POT
                 print(E, flux, file=fout)
-    return output_flux_file 
+    return output_flux_file
