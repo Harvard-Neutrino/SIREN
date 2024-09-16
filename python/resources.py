254eb2d8855e5369f617e873dafbf32fa6efa747
Austin Schneider <hogenshpogen@gmail.com>
2024-09-15 18:57:01 -0600

    Expose resource loader functions in the resources submodule

--- python/resources.py
+++ python/resources.py
@@ -1,4 +1,10 @@
+__all__ = ["load_flux", "load_detector", "load_processes", "fluxes", "detectors", "processes"]
+
 from . import _util
+
+load_flux = _util.load_flux
+load_detector = _util.load_detector
+load_processes = _util.load_processes
 
 class ResourceList:
     def __init__(self, resource_type, list_method, load_method):
@@ -24,4 +30,5 @@ fluxes = ResourceList('fluxes', _util.list_fluxes, _util._get_flux_loader)
 detectors = ResourceList('detectors', _util.list_detectors, _util._get_detector_loader)
 processes = ResourceList('processes', _util.list_processes, _util._get_process_loader)
 
+del _util
 del ResourceList
