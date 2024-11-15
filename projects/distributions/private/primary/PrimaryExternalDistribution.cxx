a79d44083df9b4697d5be44cfd8362b1122e3abc
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-11-15 18:53:30 -0500

    getting HNLs to work for SINE and UNDINE

--- projects/distributions/private/primary/PrimaryExternalDistribution.cxx
+++ projects/distributions/private/primary/PrimaryExternalDistribution.cxx
@@ -12,9 +12,9 @@ namespace distributions {
 //---------------
 // class PrimaryExternalDistribution : PrimaryExternalDistribution
 //---------------
-PrimaryExternalDistribution::PrimaryExternalDistribution(std::string _filename)
-{
-    filename = _filename;
+
+void PrimaryExternalDistribution::LoadInputFile(std::string _filename)
+{filename = _filename;
     input_file.open(filename);
     init_pos_set = false;
     mom_set = false;
@@ -43,12 +43,27 @@ PrimaryExternalDistribution::PrimaryExternalDistribution(std::string _filename)
     while (std::getline(input_file,line)) {
         std::vector<double> tmp_data;
         std::stringstream _ss(line);
+        size_t ikey = 0;
+        bool passed = true;
         while (std::getline(_ss, value, ',')) {
+            if (keys[ikey]=="E") {
+                if (stod(value) < emin) passed = false;
+            }
             tmp_data.push_back(stod(value));
         }
-        input_data.push_back(tmp_data);
+        if (passed) input_data.push_back(tmp_data);
     }
 
+}
+
+PrimaryExternalDistribution::PrimaryExternalDistribution(std::string _filename) : emin(0)
+{
+    LoadInputFile(_filename);
+}
+
+PrimaryExternalDistribution::PrimaryExternalDistribution(std::string _filename, double emin) : emin(emin)
+{
+    LoadInputFile(_filename);
 }
 
 PrimaryExternalDistribution::PrimaryExternalDistribution(PrimaryExternalDistribution const & other)
@@ -62,42 +77,54 @@ void PrimaryExternalDistribution::Sample(
         std::shared_ptr<siren::interactions::InteractionCollection const> interactions,
         siren::dataclasses::PrimaryDistributionRecord & record) const {
 
-    int i = int(rand->Uniform() * input_data.size());
-    int i_key = 0;
-    std::array<double, 3> _initial_position;
-    std::array<double, 3> _momentum;
-    for (auto value : input_data[i]) {
-        if (keys[i_key] == "x0") {
-            _initial_position[0] = value;
+    size_t max_tries = 1000;
+    size_t num_tries = 0;
+    bool success = false;
+    while(!success && num_tries < max_tries) {
+        ++num_tries;
+        int i = int(rand->Uniform() * input_data.size());
+        int i_key = 0;
+        std::array<double, 3> _initial_position;
+        std::array<double, 3> _momentum;
+        for (auto value : input_data[i]) {
+            if (keys[i_key] == "x0") {
+                _initial_position[0] = value;
+            }
+            else if (keys[i_key] == "y0") {
+                _initial_position[1] = value;
+            }
+            else if (keys[i_key] == "z0") {
+                _initial_position[2] = value;
+            }
+            else if (keys[i_key] == "px") {
+                _momentum[0] = value;
+            }
+            else if (keys[i_key] == "py") {
+                _momentum[1] = value;
+            }
+            else if (keys[i_key] == "pz") {
+                _momentum[2] = value;
+            }
+            else if (keys[i_key] == "E") {
+                if (value > emin) success=true;
+                else success=false;
+                record.SetEnergy(value);
+            }
+            else if (keys[i_key] == "m") {
+                record.SetMass(value);
+            }
+            else {
+                record.SetInteractionParameter(keys[i_key],value);
+            }
+            ++i_key;
         }
-        else if (keys[i_key] == "y0") {
-            _initial_position[1] = value;
-        }
-        else if (keys[i_key] == "z0") {
-            _initial_position[2] = value;
-        }
-        else if (keys[i_key] == "px") {
-            _momentum[0] = value;
-        }
-        else if (keys[i_key] == "py") {
-            _momentum[1] = value;
-        }
-        else if (keys[i_key] == "pz") {
-            _momentum[2] = value;
-        }
-        else if (keys[i_key] == "E") {
-            record.SetEnergy(value);
-        }
-        else if (keys[i_key] == "m") {
-            record.SetMass(value);
-        }
-        else {
-            record.SetInteractionParameter(keys[i_key],value);
-        }
-        ++i_key;
+        if(mom_set) record.SetThreeMomentum(_momentum);
+        if(init_pos_set) record.SetInitialPosition(_initial_position);
     }
-    if(mom_set) record.SetThreeMomentum(_momentum);
-    if(init_pos_set) record.SetInitialPosition(_initial_position);
+    if(!success) {
+        std::cout << "Failed to generate a physical primary after " << max_tries << "attempts. Try again\n";
+        exit(0);
+    }
 }
 
 std::vector<std::string> PrimaryExternalDistribution::DensityVariables() const {
