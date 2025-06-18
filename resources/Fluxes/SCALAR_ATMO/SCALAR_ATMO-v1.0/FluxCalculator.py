23bc2481edbbc0f39660e1dc0e533db5bf43a728
Nicholas Kamp <nkamp@fas.harvard.edu>
2025-06-18 17:06:42 -0400

    runtime error fixes, including larger fix in Decay class that properly calculates beta/gamma for an interaction recrod with on ly energy and mass sampled

--- resources/Fluxes/SCALAR_ATMO/SCALAR_ATMO-v1.0/FluxCalculator.py
+++ resources/Fluxes/SCALAR_ATMO/SCALAR_ATMO-v1.0/FluxCalculator.py
@@ -1,25 +1,24 @@
 import os
+s_per_year = 60*60*24*365.25
 def MakeFluxFile(tag, abs_flux_dir):
     '''
     Accepts the following tags:
         phi
     '''
-    mode,particle = tag.split("_")
-    if mode not in ["phi"]:
-        print("%s beam mode specified in tag %s is not valid"%(mode,tag))
+    if tag not in ["phi"]:
+        print(" tag %s is not valid"%(tag))
         exit(0)
     input_flux_file = os.path.join(abs_flux_dir,
-                                   "%s.dat"%mode)
+                                   "%s.dat"%tag)
     output_flux_file = os.path.join(abs_flux_dir,
-                                    "%s.txt"%mode)
+                                    "%s.txt"%tag)
     with open(input_flux_file,"r") as fin:
         all_lines = fin.readlines()
         headers = all_lines[0].strip().split()
         data = [line.strip().split() for line in all_lines[1:]]
-        pid = headers.index(particle)
         with open(output_flux_file,"w") as fout:
             for row in data:
                 energy,flux = float(row[0]),float(row[1])
-                flux = flux*1e4*4*3.14159/(energy**3) # put flux in units of nu/m^2/GeV/s
+                flux = flux*s_per_year*1e4*4*3.14159/(energy**3) # put flux in units of nu/m^2/GeV/yr
                 print(energy,flux,file=fout)
     return output_flux_file
